c207eff898f05eb185d807676cfeb39555da818c
mohsenataeiasad@gmail.com <mohsenataeiasad@gmail.com>
2020-01-11 18:08:09 +0330

    added some dependenceis.

---
+++ app/src/main/java/co/mohsen/mvvmsample/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package co.mohsen.mvvmsample.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.mohsen.mvvmsample.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
